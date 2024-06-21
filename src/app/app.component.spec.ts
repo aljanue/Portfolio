927ef52a4e4543a83f6789e1ec3095b1cbd3989d
aljanue <albertojativa0905@gmail.com>
2024-06-21 18:05:42 +0200

    portfolio 2.0

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'porfolio-aljanue' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('porfolio-aljanue');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ResumeComponent } from './resume/resume.component';
+import { CheckFormComponent } from './check-form/check-form.component';
+import { ProfileComponent } from './profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: AboutComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'resume', component: ResumeComponent},
+    {path: 'portfolio', component:PortfolioComponent},
+    {path: 'contact', component:ContactComponent},
+    {path: 'check-form', component:CheckFormComponent},
+    {path: 'home', component:ProfileComponent}
+];
