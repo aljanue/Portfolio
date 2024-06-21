8d4633eddcfbdf34c479d3fdbd8847b7767a54e3
aljanue <albertojativa0905@gmail.com>
2024-06-21 18:50:48 +0200

    chore: Add device detection and redirect to mobile home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProfileComponent } from './profile/profile.component';
+import { Router } from '@angular/router';
+import { DeviceDetectorService } from './device-detector.service';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,12 @@ import { ProfileComponent } from './profile/profile.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+  constructor(private router: Router, private deviceDetectorService: DeviceDetectorService) { }
+
+  ngOnInit() {
+    if (this.deviceDetectorService.isMobile()) {
+      this.router.navigate(['/home']); // Cambia '/ruta-movil' por la ruta a la que quieres redirigir en dispositivos móviles
+    }
+  }
 }
