d49f43362a17d1f979ad4d970ef7b51b51f79da7
Alberto Játiva <134947748+aljanue@users.noreply.github.com>
2026-01-18 03:00:14 +0100

    added 404 page

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css',
 })
-export class NotFoundComponent {}
+export class NotFoundComponent {
+  professionalMessages = [
+    'Page Not Found',
+    'The Requested Page Could Not Be Found',
+    'This Page Doesn\'t Exist',
+    'We Couldn\'t Find That Page',
+    'Page Unavailable',
+  ];
+
+  currentMessage: string;
+
+  constructor(private router: Router) {
+    this.currentMessage = this.professionalMessages[Math.floor(Math.random() * this.professionalMessages.length)];
+  }
+
+  goHome(): void {
+    this.router.navigate(['/']);
+  }
+}
